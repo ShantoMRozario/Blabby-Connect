95494d7fbb4d8b2626ec9966e7de9e4e9e991265
Shanto M Rozario <shantoxrozario@gmail.com>
2024-01-14 06:35:39 +0000

    Jan 14, 2024, 12:35 PM

--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
 
 
 import { configureStore } from '@reduxjs/toolkit'
+import selectedFriendSlice from './Slices/selectedFriendSlice'
 import userSlice from './Slices/userSlice'
 
 export default configureStore({
   reducer: {
-    userLoginInfo: userSlice
+    userLoginInfo: userSlice,
+    selectedFriendChat :  selectedFriendSlice
   },
 })
